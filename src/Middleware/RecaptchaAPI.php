d9cfc8f8c23b9bb4b2c945ff1b633b02aef47f3b
Ben Walandow <bentol777@yahoo.com>
2020-11-07 19:13:04 +0700

    SAVE

--- src/Middleware/RecaptchaAPI.php
+++ src/Middleware/RecaptchaAPI.php
@@ -20,39 +20,45 @@ class RecaptchaAPI
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @throws \Exception
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(!config('bendt-auth.recaptcha', false)) return $next($request);
+        if (!config('bendt-auth.recaptcha', false)) return $next($request);
 
         $captcha = $request->input('g-recaptcha-response');
+        $path = $request->path();
+        $grant_type = $request->input('grant_type');
 
-        if($captcha){
+        if ($captcha) {
             $secret = config('bendt-auth.recaptcha_secret', '');
-            $url = 'https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$captcha;
-            if( ini_get('allow_url_fopen') ) {
+            $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $captcha;
+            if (ini_get('allow_url_fopen')) {
                 $verifyResponse = file_get_contents($url);
             } else if (function_exists('curl_version')) {
                 $verifyResponse = $this->url_get_contents($url);
             } else {
-                abt_custom('Oops!','Unable to communicate with Google Recaptcha');
+                abt_custom('Oops!', 'Unable to communicate with Google Recaptcha');
             }
             $responseData = json_decode($verifyResponse);
-            if(!$responseData->success){
-                return abt_custom('Oops!','We are unable to verify you are a human or a bot.');
+            if (!$responseData->success) {
+                return abt_custom('Oops!', 'We are unable to verify you are a human or a bot.');
             }
+        } else if ($grant_type === 'refresh_token' && $path === 'oauth/token') {
+            return abt_custom('Oops!', 'We are unable to verify you are a human or a bot.');
         } else {
-            return abt_custom('Oops!','We are unable to verify you are a human or a bot.');
+            return abt_custom('Oops!', 'We are unable to verify you are a human or a bot.');
         }
 
         return $next($request);
     }
 
-    private function url_get_contents($url) {
-        if (!function_exists('curl_init')){
+    private function url_get_contents($url)
+    {
+        if (!function_exists('curl_init')) {
             die('CURL is not installed!');
         }
         $ch = curl_init();
